850ed722138144dfd39676775ea1ab248d53e2d7
poopy2121 <ggg1111g1g1g@gmail.com>
2024-10-01 12:45:12 +0200

    event listener schon mal grob compare to correct answer mit feedback

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const answerButtons = document.querySelectorAll(".answerBox");
 const clapping = new Audio(
-  "soundeffects/cheering-and-clapping-crowd-1-5995.mp3");
+  "soundeffects/cheering-and-clapping-crowd-1-5995.mp3"
+);
 const correctSound = new Audio("soundeffects/correct-6033.mp3");
 const questions = [
   {
@@ -34,15 +35,86 @@ let answerC = document.getElementById("SpanC");
 let answerD = document.getElementById("SpanD");
 
 let correctAnswer;
-
 function displayQuestion(index) {
   question.innerText = questions[index].question;
   answerA.innerText = questions[index].options[0];
   answerB.innerText = questions[index].options[1];
   answerC.innerText = questions[index].options[2];
   answerD.innerText = questions[index].options[3];
+  correctAnswer = questions[index].answer;
 }
+
+
 displayQuestion(0);
+
+
+const answerBoxA = document.getElementById("answerBoxA");
+const answerBoxB = document.getElementById("answerBoxB");
+const answerBoxC = document.getElementById("answerBoxC");
+const answerBoxD = document.getElementById("answerBoxD");
+
+// NOTE: There are 3 approaches for handling button clicks:
+// 1. Separate listeners for each button (easy but repetitive) not efficient but iam dumb  so ill pick this
+// 2. A single event handler function for all buttons (balanced)
+// 3. Using querySelectorAll with a loop (efficient but more complex)
+
+
+
+answerBoxA.addEventListener("click", function () {
+   if (correctAnswer === answerA.innerText)
+     if (correctAnswer === answerA.innerText) {
+       console.log("u won!");
+       answerBoxA.style.border = "solid green 3px";
+       correctSound.play();
+     } else {
+       answerBoxA.style.border = "solid red 2px";
+     }
+
+}
+);
+
+answerBoxB.addEventListener('click', function () {
+   if (correctAnswer === answerB.innerText)
+      {
+       console.log("u won!");
+       answerBoxB.style.border = "solid green 3px";
+       correctSound.play();
+     } else {
+       answerBoxB.style.border = "solid red 2px";
+     } 
+
+})
+
+
+answerBoxC.addEventListener("click", function () {
+   if ((correctAnswer === answerC.innerText)) {
+     console.log("u won!");
+     answerBoxC.style.border = "solid green 3px";
+     correctSound.play();
+   } 
+   else {
+    answerBoxC.style.border = "solid red 2px";
+   }
+}
+);
+
+
+answerBoxD.addEventListener("click", function () {
+   if (correctAnswer === answerD.innerText)
+     if (correctAnswer === answerD.innerText) {
+       console.log("u won!");
+       answerBoxD.style.border = "solid green 3px";
+       correctSound.play();
+     } else {
+       answerBoxD.style.border = "solid red 2px";
+     }
+}
+);
+
+
+
+
+
 
 // const nextQuestionBtn = document.createElement('button');
 
